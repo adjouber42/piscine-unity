7d2b9effd238c7247326f6a00f73f11f4156a962
Adrien JOUBERT <adjouber@e2r10p13.42.fr>
2020-02-27 13:40:32 +0100

    d00 ex00

---
+++ d00/ex00/Assets/Balloon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public int jauge = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (jauge < 5)
+            {
+                transform.localScale = new Vector3 (transform.localScale[0] + 0.2f, transform.localScale[1] + 0.2f, transform.localScale[2] + 0.2f);
+                jauge++;
+            }
+        }
+        if (Mathf.RoundToInt(10.0f * Time.realtimeSinceStartup % 8) == 0)
+        {
+            if (jauge > 0)
+                jauge--;
+        }
+        if (Mathf.RoundToInt(10.0f * Time.realtimeSinceStartup % 5) == 0)
+            transform.localScale = new Vector3 (transform.localScale[0] - 0.05f, transform.localScale[1] - 0.05f, transform.localScale[2] - 0.05f);
+        if (transform.localScale[0] > 5.0f || transform.localScale[0] <= 0.0f)
+        {
+            Destroy(gameObject);
+            print("Balloon life time: " + Mathf.RoundToInt(Time.realtimeSinceStartup) + "s");
+        }
+
+    }
+}
