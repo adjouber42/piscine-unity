a41645f0247667ab7c5ace38ab3084f2091c3c65
Adrien JOUBERT <adjouber@e1r11p11.42.fr>
2020-02-28 15:39:17 +0100

    d00 ex01

---
+++ d00/ex01/Assets/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(-0.05f, -0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, speed / 2, 0));
+    }
+}
---
+++ d00/ex01/Assets/CubeSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    int i;
+    private GameObject tt;
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        i = Random.Range(0, 50);
+        if (!tt && i == 10 && prefab)
+        {
+            tt = (GameObject)GameObject.Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+        if (tt && tt.transform.position.y < -5)
+            Destroy(tt);
+        if (tt && Input.GetKeyDown("a") && prefab.name == "A")
+        {
+            Debug.Log("Precision: " + ((tt.transform.position.y - 5) * -10));
+            Destroy(tt);
+        }
+        if (tt && Input.GetKeyDown("s") && prefab.name == "S")
+        {
+            Debug.Log("Precision: " + ((tt.transform.position.y - 5) * -10));
+            Destroy(tt);
+        }
+        if (tt && Input.GetKeyDown("d") && prefab.name == "D")
+        {
+            Debug.Log("Precision: " + ((tt.transform.position.y - 5) * -10));
+            Destroy(tt);
+        }
+    }
+}
