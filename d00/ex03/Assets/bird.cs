59ee7b4ad56d27e0f9c0646865fe761d87abe0b4
Adrien JOUBERT <adjouber@e2r10p15.42.fr>
2020-03-02 15:13:26 +0100

    ex03

---
+++ d00/ex03/Assets/bird.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    private bool play = true;
+    private int i = 0;
+    private float   time = 0;
+    public int  score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play)
+        {
+            time += Time.deltaTime;
+            if (Input.GetKeyDown("space"))
+                i = 10;
+            if (i != 0)
+            {
+                transform.Translate(new Vector3(0.0f, 0.1f, 0.0f));
+                i--;
+            }
+            else
+                transform.Translate(new Vector3(0.0f, -0.01f, 0.0f));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Not Ok")
+        {
+            Debug.Log ("Score: " + score);
+            Debug.Log ("Time: " + Mathf.RoundToInt(time) + "s");
+            play = false;
+        }
+        else if (coll.gameObject.tag == "Ok")
+            score += 5;
+        else
+            Debug.Log("?");
+    }
+}
