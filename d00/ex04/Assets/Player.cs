ccf22aa7e90879167b6a130eb0cb1873dacd82b5
Adrien JOUBERT <adjouber@e2r10p15.42.fr>
2020-03-05 13:20:09 +0100

    d00 ex04

---
+++ d00/ex04/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag == "Player1")
+        {
+            if (Input.GetKey("w") && transform.position.y < 2.9)
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.02f, transform.position.z);
+            if (Input.GetKey("s") && transform.position.y > -2.9)
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.02f, transform.position.z);
+        }
+        if (gameObject.tag == "Player2")
+        {
+            if (Input.GetKey("up") && transform.position.y < 2.9)
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.02f, transform.position.z);
+            if (Input.GetKey("down") && transform.position.y > -2.9)
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.02f, transform.position.z);
+        }
+    }
+}
---
+++ d00/ex04/Assets/PongBall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour
+{
+    public float    x;
+    public float    y;
+    public int    scoreP1 = 0;
+    public int    scoreP2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0.02f;
+        if (Random.Range(-1.0f, 1.0f) < 0.0f)
+            x = x * -1;
+        y = Random.Range(-0.03f, 0.03f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(x, y, 0.0f));
+        if (transform.position.y > 3.5f || transform.position.y < -3.5f)
+            y = -1 * y;
+        if (transform.position.x < -6.5f)
+        {
+            scoreP1++;
+            Debug.Log("Player 1: " + scoreP1 + " | Player 2: " + scoreP2);
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            x = 0.02f;
+            if (Random.Range(-1.0f, 1.0f) < 0.0f)
+                x = x * -1;
+            y = Random.Range(-0.03f, 0.03f);
+        }
+        if (transform.position.x > 6.5f)
+        {
+            scoreP2++;
+            Debug.Log("Player 1: " + scoreP1 + " | Player 2: " + scoreP2);
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            x = 0.02f;
+            if (Random.Range(-1.0f, 1.0f) < 0.0f)
+                x = x * -1;
+            y = Random.Range(-0.03f, 0.03f);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player1" || coll.gameObject.tag == "Player2")
+            x = -1 * x;
+    }
+}
